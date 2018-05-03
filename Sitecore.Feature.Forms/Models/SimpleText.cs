c3495b125d097d35b2a223252cf2f63e65894810
Mickey Rahman <rahm0277@gmail.com>
2018-05-02 22:31:46 -0400

    Added confirmation message for form

---
+++ Sitecore.Feature.Forms/Models/SimpleText.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.Forms.Models
+{
+    public class SimpleText : IRenderingModel
+    {
+
+        Item _item;
+
+        public void Initialize(Mvc.Presentation.Rendering rendering)
+        {
+            _item = rendering.Item;
+        }
+
+        public string TextBody
+        {
+            get
+            {
+                return FieldRenderer.Render(_item, "Text Body");
+            }
+        }
+
+        
+    }
+}
