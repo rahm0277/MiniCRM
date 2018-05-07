f367b3d8fdcf83aecbd6d9402e504da762e753ab
Mickey Rahman <rahm0277@gmail.com>
2018-05-07 12:13:32 -0400

    Rearranged everything

---
+++ Foundation/CustomView/Sitecore.Foundation.CustomView/BaseView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Foundation.CustomView
+{
+    public abstract class BaseView<T> : WebViewPage<T>
+    {
+        /// <summary>
+        /// Property to get the dictionary domain, as set in settings. Also set at the <sites> node level for the site definition
+        /// </summary>
+        public string DictionaryDomain
+        {
+            get
+            {
+                return Sitecore.Configuration.Settings.GetSetting("Sitecore.Foundation.Dictionary.Domain");
+            }
+        }
+    }
+}
---
+++ Foundation/CustomView/Sitecore.Foundation.CustomView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sitecore.Foundation.CustomView.MvcApplication" Language="C#" %>
